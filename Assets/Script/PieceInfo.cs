b2fc00a48e701902c483dd6bfa3574956a6b6135
SlayerWx <maxi_cs_95@hotmail.com>
2021-10-22 16:28:13 -0300

    add pieceInfo to get piece reference

---
+++ Assets/Script/PieceInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PieceInfo : MonoBehaviour
+{
+    public Vector3 positionInCubeID; //left to right,Top to Bot,front to back
+
+    private void Start()
+    {
+        BlockPositions.OnSetBlockPosition?.Invoke(this);
+    }
+}

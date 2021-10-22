eb6c3981e3efc7b52cbd81bb87d26a5fb7af2540
SlayerWx <maxi_cs_95@hotmail.com>
2021-10-22 16:28:56 -0300

    add action gyroscope

--- Assets/Script/RotateGyroscope.cs
+++ Assets/Script/RotateGyroscope.cs
@@ -2,20 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
+using System;
 public class RotateGyroscope : MonoBehaviour
 {
 
     private Gyroscope phoneGyro;
     private Quaternion correctionQuaternion;
-
     void Start()
     {
         phoneGyro = Input.gyro;
         phoneGyro.enabled = true;
         correctionQuaternion = Quaternion.Euler(90f, 0f, 0f);
     }
-
+    private void OnEnable()
+    {
+        InputPlayer.OnGyroscopeRotatationEuler += GyroRotateEuler;
+    }
+    private void OnDisable()
+    {
+        InputPlayer.OnGyroscopeRotatationEuler -= GyroRotateEuler;
+    }
     void Update()
     {
         GyroModifyCamera();
@@ -27,10 +33,22 @@ public class RotateGyroscope : MonoBehaviour
         // rotate coordinate system 90 degrees. Correction Quaternion has to come first
         Quaternion calculatedRotation = correctionQuaternion * gyroQuaternion;
         transform.rotation = calculatedRotation;
+        
     }
 
     private static Quaternion GyroToUnity(Quaternion q)
     {
         return new Quaternion(q.x, q.y, -q.z, -q.w);
+    }
+    Vector3 GyroRotateEuler()
+    {
+        return transform.rotation.eulerAngles;
+    }
+    protected void OnGUI()
+    {
+        //GUI.skin.label.fontSize = Screen.width / 40;
+
+        //GUILayout.Label("Orientation: " + Screen.orientation);
+        //GUILayout.Label("euler: " + transform.rotation.eulerAngles);
     }
 }
